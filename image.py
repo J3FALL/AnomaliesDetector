990e3e21aaeb0b402b8b0d7b5513790346181e4c
J3FALL <crisp.coad@gmail.com>
2017-12-20 19:04:21 +0300

    Add save/load square functions

--- image.py
+++ image.py
@@ -2,6 +2,15 @@ import numpy as np
 from netCDF4 import Dataset
 
 
+def save_square_to_file(square, file_name):
+    np.save(file_name + '.npy', square)
+
+
+def load_square_from_file(file_name):
+    square = np.load(file_name + '.npy')
+    return square
+
+
 class NCImage:
     def __init__(self, file_name):
         self.file_name = file_name
@@ -11,11 +20,13 @@ class NCImage:
         return np.array(self.dataset.variables[var])
 
     def extract_square(self, var, x_offset, y_offset, size, depth=0, time=0):
-
         return self.extract_variable(var)[time, depth, x_offset:x_offset + size, y_offset:y_offset + size]
 
 
 image = NCImage("samples/uv_sample_with_outliers.nc")
 
 square = image.extract_square('vozocrtx', 0, 0, 50, 0, 0)
-print(square)
+save_square_to_file(square, 'test')
+
+test = load_square_from_file('test')
+
