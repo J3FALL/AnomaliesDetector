e0477f8247c0a209a1fe2bea3fbb7781b95aa848
J3FALL <crisp.coad@gmail.com>
2017-12-20 18:38:38 +0300

    Add extract_square function

--- image.py
+++ image.py
@@ -10,8 +10,12 @@ class NCImage:
     def extract_variable(self, var):
         return np.array(self.dataset.variables[var])
 
+    def extract_square(self, var, x_offset, y_offset, size, depth=0, time=0):
+
+        return self.extract_variable(var)[time, depth, x_offset:x_offset + size, y_offset:y_offset + size]
+
 
 image = NCImage("samples/uv_sample_with_outliers.nc")
 
-print(image.extract_variable('vozocrtx'))
-
+square = image.extract_square('vozocrtx', 0, 0, 50, 0, 0)
+print(square)
