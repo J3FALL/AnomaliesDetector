a5d3d7fedc1babaffe06db7b05db0c91afddad36
J3FALL <crisp.coad@gmail.com>
2017-12-20 18:02:12 +0300

    Add extract_variable function

--- image.py
+++ image.py
@@ -1,6 +1,17 @@
+import numpy as np
+from netCDF4 import Dataset
+
 
 class NCImage:
     def __init__(self, file_name):
         self.file_name = file_name
+        self.dataset = Dataset(filename=file_name, mode='r')
 
-    
+    def extract_variable(self, var):
+        return np.array(self.dataset.variables[var])
+
+
+image = NCImage("samples/uv_sample_with_outliers.nc")
+
+print(image.extract_variable('vozocrtx'))
+
