d2e2224071b6761fbfbc7e07a583bc42f30d54fa
J3FALL <crisp.coad@gmail.com>
2018-01-16 16:46:15 +0300

    Dump last version
    Gpu enabled

--- model.py
+++ model.py
@@ -1,13 +1,12 @@
 import csv
 
 import keras
-import matplotlib.pyplot as plt
 import numpy as np
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, Dropout
 from keras.models import Sequential
-from sklearn.model_selection import train_test_split
-
+from keras.optimizers import SGD
+from keras.optimizers import Adam
 import image
 
 
@@ -18,6 +17,10 @@ def read_samples(file_name):
         next(reader, None)
         for row in reader:
             row[1] = int(row[1])
+            square = image.load_square_from_file(row[0])
+            if square.max() == 0.0 and square.min() == 0.0:
+                next(reader)
+
             samples.append(row)
 
     for sample in samples:
@@ -25,42 +28,63 @@ def read_samples(file_name):
         label[sample[1]] = 1
         sample[1] = label
 
-    return samples
+    a, b = divmod(len(samples), 50)
+
+    return samples[0:len(samples) - b]
 
 
 def split_data():
     good_samples_file = "samples/good_samples.csv"
     good_samples = read_samples(good_samples_file)
 
-    train_good, test_good = train_test_split(good_samples, test_size=0.2)
+    # train_good, test_good = train_test_split(good_samples, test_size=0.2)
 
     bad_samples_file = "samples/bad_samples.csv"
     bad_samples = read_samples(bad_samples_file)
 
-    train_bad, test_bad = train_test_split(bad_samples, test_size=0.2)
+    # train_bad, test_bad = train_test_split(bad_samples, test_size=0.2)
 
-    train = train_good + train_bad
-    test = test_good + test_bad
+    train = bad_samples
+    test = bad_samples
 
     return train, test
 
 
 def generate_train_data(samples):
     while 1:
-        batch_size = int(len(samples) / 11)
+        batch_size = int(len(samples) / 50)
         for sample_index in range(0, len(samples), batch_size):
             x = np.zeros((batch_size, 100, 100, 1), dtype=np.float32)
             y = np.zeros((batch_size, 2))
             for index in range(sample_index, sample_index + batch_size):
                 file_name = samples[index][0]
                 square = image.load_square_from_file(file_name)
-                expanded = np.expand_dims(square, axis=2)
+                rows_sum = square.sum(axis=1)
+                new_matrix = square / rows_sum[:, np.newaxis]
+                expanded = np.expand_dims(new_matrix, axis=2)
                 x[index - sample_index] = expanded
                 y[index - sample_index] = samples[index][1]
             yield (x, y)
 
 
 def generate_test_data(samples):
+    while 1:
+        batch_size = int(len(samples) / 25)
+        for sample_index in range(0, len(samples), batch_size):
+            x = np.zeros((batch_size, 100, 100, 1), dtype=np.float32)
+            y = np.zeros((batch_size, 2))
+            for index in range(sample_index, sample_index + batch_size):
+                file_name = samples[index][0]
+                square = image.load_square_from_file(file_name)
+                rows_sum = square.sum(axis=1)
+                new_matrix = square / rows_sum[:, np.newaxis]
+                expanded = np.expand_dims(new_matrix, axis=2)
+                x[index - sample_index] = expanded
+                y[index - sample_index] = samples[index][1]
+            yield (x, y)
+
+
+def generate_test_outliers(samples):
     while 1:
         batch_size = int(len(samples) / 10)
         for sample_index in range(0, len(samples), batch_size):
@@ -69,27 +93,48 @@ def generate_test_data(samples):
             for index in range(sample_index, sample_index + batch_size):
                 file_name = samples[index][0]
                 square = image.load_square_from_file(file_name)
-                expanded = np.expand_dims(square, axis=2)
+                rows_sum = square.sum(axis=1)
+                new_matrix = square / rows_sum[:, np.newaxis]
+                expanded = np.expand_dims(new_matrix, axis=2)
                 x[index - sample_index] = expanded
                 y[index - sample_index] = samples[index][1]
             yield (x, y)
+
+
+def get_validation_data():
+    valid_samples_file = "samples/rest_bad_samples.csv"
+    samples = read_samples(valid_samples_file)
+
+    return samples
+
+
+def get_outliers_validation_data():
+    file = "samples/valid_samples.csv"
+    samples = read_samples(file)
+    result = []
+    for sample in samples:
+        if sample[1][1] == 1:
+            result.append(sample)
+
+    return result[0:240]
 
 
 train, test = split_data()
 input_shape = (100, 100, 1)
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
-                 activation='relu',
+                 activation='sigmoid',
                  input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Conv2D(64, (5, 5), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(1000, activation='relu'))
-model.add(Dense(2, activation='softmax'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='sigmoid'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adam(),
+model.compile(loss="binary_crossentropy",
+              optimizer="rmsprop",
               metrics=['accuracy'])
 
 
@@ -101,24 +146,30 @@ class AccuracyHistory(keras.callbacks.Callback):
         self.acc.append(logs.get('acc'))
 
 
+test = get_validation_data()
+
 print(len(train))
 print(len(test))
+out = get_outliers_validation_data()
+print(len(out))
 history = AccuracyHistory()
-train_batch_size = int(len(train) / 11)
-test_batch_size = int(len(test) / 5)
+train_batch_size = int(len(train) / 50)
+test_batch_size = int(len(test) / 25)
 epochs = 10
 
 model.fit_generator(generate_train_data(train),
                     samples_per_epoch=train_batch_size,
                     nb_epoch=epochs,
+                    shuffle=True,
                     callbacks=[history],
-                    validation_data=generate_test_data(test),
-                    validation_steps=int(len(test) / test_batch_size))
+                    validation_data=generate_test_outliers(out),
+                    validation_steps=10)
 
 model.save("model.h5")
 
-score = model.evaluate_generator(generate_test_data(test), workers=4)
-plt.plot(range(1, 10), history.acc)
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.show()
+score = model.evaluate_generator(generate_test_outliers(out), steps=5)
+print(score)
+# plt.plot(range(1, 10), history.acc)
+# plt.xlabel('Epochs')
+# plt.ylabel('Accuracy')
+# plt.show()
